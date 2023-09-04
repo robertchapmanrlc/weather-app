0e8a8daf70870682c2f03a242838d69618d0b0cd
Robert Chapman <chapmanrobert12@gmail.com>
2023-09-04 00:09:06 -0500

    fetch API call used to retrieve weather information from openweather API

--- src/components/WeatherApp.tsx
+++ src/components/WeatherApp.tsx
@@ -1,9 +1,38 @@
+import { useEffect, useState } from "react";
 
+const api = {
+  key: "",
+  base: "",
+};
 
 function WeatherApp() {
-  return (
-    <div>WeatherApp</div>
-  )
+  const [lat, setLat] = useState(0);
+  const [long, setLong] = useState(0);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      console.log(lat, long);
+
+      await fetch(
+        `${api.base}/weather?lat=${lat}&lon=${long}&units=metric&APPID=${api.key}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          console.log(result);
+          setData(result);
+        })
+        .catch((err) => console.log(err.message));
+    };
+    fetchData();
+  }, [lat, long]);
+
+  return <></>;
 }
 
-export default WeatherApp
+export default WeatherApp;
