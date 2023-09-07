de34b130e15c0ce6dee00c01089ab9ae0da81c95
Robert Chapman <chapmanrobert12@gmail.com>
2023-09-06 23:34:32 -0500

    Progress made on stylizing the app

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -28,53 +28,58 @@ interface Props {
 function Weather({ weatherData }: Props) {
   return (
     <>
-      <Card>
+      <Card className="card-daytime" sx={{color: 'white', minWidth: 500, minHeight: 500}}>
         <CardContent>
-          <CardHeader className="header" title={weatherData.name} />
+          <CardHeader
+            className="header"
+            title={weatherData.name}
+          />
           <Typography variant="body1">
             Temperature: {weatherData.main.temp} &deg;F
           </Typography>
-          <Typography variant="body1">
+          <Typography variant="body1" >
             Max Temp: {weatherData.main.temp_max} &deg;F
           </Typography>
-          <Typography variant="body1">
+          <Typography variant="body1" >
             Min Temp: {weatherData.main.temp_min} &deg;F
           </Typography>
-          <Typography variant="body1">
+          <Typography variant="body1" >
             Sunrise:{" "}
             {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString(
               "en-IN"
             )}
           </Typography>
-          <Typography variant="body1">
+          <Typography variant="body1" >
             Sunset:{" "}
             {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString(
               "en-IN"
             )}
           </Typography>
-          <Typography variant="body1">
+          <Typography variant="body1" >
             Description: {weatherData.weather[0].description}
           </Typography>
-          <Typography variant="body1">
+          <Typography variant="body1" >
             Humidity: {weatherData.main.humidity}%
           </Typography>
-          <Typography variant="body1">
+          <Typography variant="body1" >
             Wind: {weatherData.wind.speed}
           </Typography>
           {weatherData.rain && (
-            <Typography variant="body1">
+            <Typography variant="body1" >
               Precipitation: {weatherData.rain["h1"]}
             </Typography>
           )}
           {weatherData.snow && (
-            <Typography variant="body1">
+            <Typography variant="body1" >
               Snow: {weatherData.snow["h1"]}
             </Typography>
           )}
-          <Typography variant="body1">
+          <Typography variant="body1" >
             Day: {moment().format("dddd")}
           </Typography>
-          <Typography variant="body1">Date: {moment().format("LL")}</Typography>
+          <Typography variant="body1" >
+            Date: {moment().format("LL")}
+          </Typography>
         </CardContent>
       </Card>
     </>
