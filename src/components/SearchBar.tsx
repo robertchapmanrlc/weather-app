a62ef7fe2d5a1f4868a48b6f2e25787fa0a2e9d9
Robert Chapman <chapmanrobert12@gmail.com>
2024-01-28 19:41:59 -0600

    The width of the search bar is now responsive. Aria label for the input field add for accessibility

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -53,7 +53,7 @@ export default function SearchBar() {
 
   return (
     <div
-      className={`w-[40%] h-10 px-4 bg-black ${
+      className={`w-full sm:max-w-[400px] md:max-w-[500px] h-10 px-4 bg-black ${
         !isFocused ? "bg-opacity-25" : "bg-opacity-75"
       } flex rounded-md transition-bg duration-300`}
     >
@@ -61,6 +61,7 @@ export default function SearchBar() {
       <input
         type="text"
         placeholder="Enter Location"
+        aria-label="Search for a location"
         className="w-full h-full bg-transparent pl-3 text-white/50 text-xl placeholder:text-white/50 outline-none transition-opacity duration-300 focus:text-white"
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
