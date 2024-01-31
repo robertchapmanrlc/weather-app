3c5a674759b773b8eb5fced32b763426f4d6c0b0
Robert Chapman <chapmanrobert12@gmail.com>
2024-01-30 21:04:48 -0600

    Starting the design of the main weather component. Created the background of the card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import MainWeather from "../components/MainWeather";
 import MobileSearchBar from "../components/MobileSearchBar";
 import SearchBar from "../components/SearchBar";
 
 export default function Main() {
   return (
-    <main className="w-full flex flex-col pt-[40px] justify-start items-center">
+    <main className="w-full flex flex-col pt-[40px] justify-start items-center gap-y-10">
       <SearchBar />
       <MobileSearchBar />
+      <MainWeather />
     </main>
   );
 }
