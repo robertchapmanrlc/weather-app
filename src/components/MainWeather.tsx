eda80a14ebb9b4123f255b0145227c072844e37b
Robert Chapman <chapmanrobert12@gmail.com>
2024-01-30 22:08:47 -0600

    Stylized the main weather content card

--- src/components/MainWeather.tsx
+++ src/components/MainWeather.tsx
@@ -7,25 +7,25 @@ import WindSpeedIcon from './../assets/Wind Speed.svg';
 
 export default function MainWeather() {
   return (
-    <div className="sm:min-w-[450px] h-[400px] bg-black/25 flex flex-col items-center justify-start rounded-xl drop-shadow-[0_30px_10px_rgba(0,0,0,0.1)]">
-      <h2>Chicao, IL</h2>
-      <Image width={100} height={100} src={SunIcon} alt="Sun Icon" />
-      <h1>72 °F</h1>
+    <div className="sm:min-w-[450px] h-[400px] pt-5 bg-transparent sm:bg-black/25 flex flex-col gap-y-6 items-center justify-start rounded-xl drop-shadow-[0_30px_10px_rgba(0,0,0,0.1)]">
+      <h2 className="hidden sm:block text-3xl text-white">Chicago, IL</h2>
+      <Image width={120} height={120} src={SunIcon} alt="Sun Icon" />
+      <h1 className="font-bold text-5xl text-white">72 °F</h1>
       <div className="w-[80%] flex flex-row justify-between items-start bg-black/25 sm:bg-transparent">
         <div className="flex flex-row gap-x-3">
           <Image width={20} height={20} src={PrecipitationIcon} alt="Precipitation Icon"/>
-          <h5>24%</h5>
+          <h5 className="font-light text-white">24%</h5>
         </div>
         <div className="flex flex-row gap-x-3">
           <Image width={20} height={20} src={HumidityIcon} alt="Humidity Icon"/>
-          <h5>24%</h5>
+          <h5 className="font-light text-white">24%</h5>
         </div>
         <div className="flex flex-row gap-x-3">
           <Image width={20} height={20} src={WindSpeedIcon} alt="Wind Speed Icon"/>
-          <h5>24%</h5>
+          <h5 className="font-light text-white">24%</h5>
         </div>
       </div>
-      <h4>Mostly Sunny</h4>
+      <h4 className="text-2xl text-white opacity-70">Mostly Sunny</h4>
     </div>
   );
 }
