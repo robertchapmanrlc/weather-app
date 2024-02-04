bb8663fa0d8edf2bf7b618e3f9ce04b2c4b62773
Robert Chapman <chapmanrobert12@gmail.com>
2024-02-03 23:26:09 -0600

    Made the Daily Forecast more responsive

--- src/components/DailyForecast.tsx
+++ src/components/DailyForecast.tsx
@@ -9,41 +9,45 @@ export default function DailyForecast() {
       <h2 className="text-white text-2xl font-bold">Forecast</h2>
       <div className="w-full h-[85%] flex flex-col md:flex-row justify-between">
         <div className="w-full md:w-24 h-36 md:h-28 flex md:flex-col items-center justify-between rounded-md">
-          <h5 className="w-28 text-white/60 text-lg">Monday</h5>
+          <h5 className="w-28 text-white/60 md:text-center text-lg">Monday</h5>
           <Image width={35} height={35} src={CloudyIcon} alt="Cloudy Icon" />
-          <div className=" w-28 flex justify-between">
+          <div className="w-28 md:w-24 flex justify-between">
             <h5 className="text-white text-lg">66 °F</h5>
             <h5 className="text-white/60 text-lg">64 °F</h5>
           </div>
         </div>
         <div className="w-full md:w-24 h-36 md:h-28 flex md:flex-col items-center justify-between rounded-md">
-          <h5 className="w-28 text-white/60 text-lg">Tuesday</h5>
+          <h5 className="w-28 text-white/60 md:text-center text-lg">Tuesday</h5>
           <Image width={35} height={35} src={SunIcon} alt="Cloudy Icon" />
-          <div className=" w-28 flex justify-between">
+          <div className="w-28 md:w-24 flex justify-between">
             <h5 className="text-white text-lg">70 °F</h5>
             <h5 className="text-white/60 text-lg">68 °F</h5>
           </div>
         </div>
         <div className="w-full md:w-24 h-36 md:h-28 flex md:flex-col items-center justify-between rounded-md">
-          <h5 className="w-28 text-white/60 text-lg">Wednesday</h5>
+          <h5 className="w-28 text-white/60 md:text-center text-lg">
+            Wednesday
+          </h5>
           <Image width={35} height={35} src={CloudyIcon} alt="Cloudy Icon" />
-          <div className=" w-28 flex justify-between">
+          <div className="w-28 md:w-24 flex justify-between">
             <h5 className="text-white text-lg">74 °F</h5>
             <h5 className="text-white/60 text-lg">70 °F</h5>
           </div>
         </div>
         <div className="w-full md:w-24 h-36 md:h-28 flex md:flex-col items-center justify-between rounded-md">
-          <h5 className="w-28 text-white/60 text-lg">Thursday</h5>
+          <h5 className="w-28 text-white/60 md:text-center text-lg">
+            Thursday
+          </h5>
           <Image width={35} height={35} src={CloudyIcon} alt="Cloudy Icon" />
-          <div className=" w-28 flex justify-between">
+          <div className="w-28 md:w-24 flex justify-between">
             <h5 className="text-white text-lg">72 °F</h5>
             <h5 className="text-white/60 text-lg">69 °F</h5>
           </div>
         </div>
         <div className="w-full md:w-24 h-36 md:h-28 flex flex-row md:flex-col items-center justify-between rounded-md">
-          <h5 className="w-28 text-white/60 text-lg">Friday</h5>
+          <h5 className="w-28 text-white/60 md:text-center text-lg">Friday</h5>
           <Image width={35} height={35} src={SunIcon} alt="Cloudy Icon" />
-          <div className=" w-28 flex justify-between">
+          <div className="w-28 md:w-24 flex justify-between">
             <h5 className="text-white text-lg">68 °F</h5>
             <h5 className="text-white/60 text-lg">64 °F</h5>
           </div>
