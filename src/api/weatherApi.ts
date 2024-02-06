ec0b43992019d999cf351bc15414e01c550cb88e
Robert Chapman <chapmanrobert12@gmail.com>
2024-02-05 23:36:01 -0600

    Added Type safety of the OpenWeather API response in the api route

--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -1,3 +1,26 @@
+interface WeatherApiResponse {
+  cod: number;
+  main: {
+    temp: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
+  };
+  rain: { [key: string]: number };
+  weather: {
+    description: string;
+  }[];
+}
+
+interface ExtractedWeatherInfo {
+  temperature: number;
+  humidity: number;
+  windSpeed: number;
+  precipitation: number | null;
+  weatherDescription: string | null;
+}
+
 export async function getLocationWeather() {
   const res = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=imperial&appid=${process.env.OPEN_WEATHER_API_KEY}`
@@ -17,4 +40,31 @@ export async function getLocationWeather() {
   }
 
   return data;
+}
+
+function extractWeatherInfo(apiResponse: WeatherApiResponse) {
+  if (!apiResponse || apiResponse.cod !== 200) {
+    throw new Error("Invalid API Response");
+  }
+
+  const {
+    main: { temp, humidity },
+    wind: { speed },
+    weather,
+    rain
+  } = apiResponse;
+
+  const mainWeather = weather && weather.length > 0 ? weather[0] : null;
+  const weatherDescription = mainWeather ? mainWeather.description : null;
+  const precipitation = rain ? rain["1h"] : null;
+
+  const extractedInfo: ExtractedWeatherInfo = {
+    temperature: temp,
+    humidity,
+    windSpeed: speed,
+    precipitation,
+    weatherDescription
+  };
+
+  return extractedInfo;
 }
