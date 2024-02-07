e307e7b1bbea6904e12105dab97ccd8c9ff8621f
Robert Chapman <chapmanrobert12@gmail.com>
2024-02-06 22:50:08 -0600

    Create api route to retrieve information from the 5 day/3 hour api

---
+++ src/app/api/forecast/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const location = request.nextUrl.searchParams;
+
+  const lat = location.get("lat");
+  const lon = location.get("lon");
+
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${process.env.OPEN_WEATHER_API_KEY}`
+  );
+
+  if (!res.ok) {
+    const errorMessage = `Failed to fetch data. Status: ${res.status}, ${res.statusText}`;
+    throw new Error(errorMessage);
+  }
+
+  const data = await res.json();
+
+  if (data.cod !== "200") {
+    throw new Error(
+      `Failed to fetch data. Cod: ${data.cod}, Message: ${data.message}`
+    );
+  }
+
+  return NextResponse.json(data);
+}
