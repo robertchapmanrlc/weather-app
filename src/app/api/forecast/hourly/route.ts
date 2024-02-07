88b9fef58291950664ef1d627cb055db77f6ef4e
Robert Chapman <chapmanrobert12@gmail.com>
2024-02-07 01:15:16 -0600

    Modified the hourly forecast endpoint to request 2 days if the time query parameter is greater than 15 - if the time is past 3pm

--- src/app/api/forecast/hourly/route.ts
+++ src/app/api/forecast/hourly/route.ts
@@ -7,9 +7,13 @@ export async function GET(request: NextRequest) {
   const lat = query.get("lat");
   const lon = query.get("lon");
   const time = Number(query.get("time"));
+  let days = 1;
+  if (time >= 15) {
+    days = 2
+  }
 
   const res = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${lat},${lon}&days=1&aqi=no&alerts=no`
+    `https://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${lat},${lon}&days=${days}&aqi=no&alerts=no`
   );
 
   if (!res.ok) {
