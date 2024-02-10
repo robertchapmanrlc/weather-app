ed74d919bcb19f366205a2e96300dc5a459098fb
Robert Chapman <chapmanrobert12@gmail.com>
2024-02-10 12:26:43 -0600

    Polished the UI for the hourly forecast component to be more responsive

--- src/components/HourlyForecast.tsx
+++ src/components/HourlyForecast.tsx
@@ -19,7 +19,7 @@ export default async function HourlyForecast() {
   const data: ExtractedHourlyForecastInfo[] = await res.json();
 
   return (
-    <div className="w-full md:max-w-[325px] h-56 md:h-[400px] px-7 py-5 flex flex-col justify-between gap-y-5 bg-black/25 rounded-xl drop-shadow-[0_30px_10px_rgba(0,0,0,0.1)]">
+    <div className="w-full md:max-w-[325px] h-52 md:h-[400px] px-7 py-5 flex flex-col justify-between gap-y-5 bg-black/25 rounded-xl drop-shadow-[0_30px_10px_rgba(0,0,0,0.1)]">
       <div className="w-full flex justify-between">
         <h2 className="text-white text-2xl font-bold">Today</h2>
         <h2 className="text-white text-2xl">{dateString}</h2>
@@ -28,18 +28,18 @@ export default async function HourlyForecast() {
         {data.map((forecast, i) => (
           <div
             key={i}
-            className={`w-full ${
+            className={`w-20 md:w-full ${
               i > 0 && "border-transparent"
-            } border-2 px-2 md:h-10 flex flex-col md:flex-row items-center justify-between rounded-md`}
+            } border-2 px-2 py-2 md:h-10 flex flex-col md:flex-row items-center justify-between rounded-md`}
           >
-            <h5 className="text-white text-lg w-14">{forecast.temp} °F</h5>
+            <h5 className="text-white md:text-lg text-center md:text-left w-full md:w-14">{forecast.temp} °F</h5>
             <Image
               width={35}
               height={35}
               src={`/${forecast.icon}.png`}
               alt={`${forecast.icon} Icon`}
             />
-            <h5 className="text-white/60 text-lg text-right w-20">
+            <h5 className="text-white/60 text-sm md:text-lg text-center md:text-right w-20">
               {forecast.time}
             </h5>
           </div>
