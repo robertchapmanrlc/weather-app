f98a859c04b3aeed0fc7f0f8e38c81dcb9c91f70
Robert Chapman <chapmanrobert12@gmail.com>
2024-02-15 11:31:44 -0600

    Removed the location state variable and made a call to the weather info api route to pass weatherData to the MainWeather component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,29 @@
-"use client";
-
-import { useState } from "react";
 import DailyForecast from "../components/DailyForecast";
 import HourlyForecast from "../components/HourlyForecast";
 import MainWeather from "../components/MainWeather";
 import MobileSearchBar from "../components/MobileSearchBar";
 import SearchBar from "../components/SearchBar";
-import { LocationCoords } from "../types/weatherTypes";
+import { ExtractedWeatherInfo, LocationCoords } from "../types/weatherTypes";
 
-export default function Main() {
+export default async function Main() {
 
-  const [location, setLocation] = useState<LocationCoords>({
-    latitude: 0,
-    longitude: 0,
-  });
+  const location: LocationCoords = {
+    latitude: 41.87,
+    longitude: -87.72
+  }
 
-  console.log(location);
+  const weatherRes = await fetch(`http://localhost:3000/api/weather?lat=${location.latitude}&lon=${location.longitude}`);
+
+  const data: ExtractedWeatherInfo = await weatherRes.json();
+
+
 
   return (
     <main className="w-full flex flex-col py-[40px] px-7 md:px-14 justify-start items-center gap-y-10">
       <SearchBar />
       <MobileSearchBar />
       <div className="w-full flex flex-col md:flex-row-reverse justify-center items-center md:gap-x-8 lg:gap-x-16">
-        <MainWeather />
+        <MainWeather weatherData={data} />
         <HourlyForecast />
       </div>
       <DailyForecast />
--- src/components/MainWeather.tsx
+++ src/components/MainWeather.tsx
@@ -1,14 +1,19 @@
 
 import Image from "next/image";
+import { ExtractedWeatherInfo } from "../types/weatherTypes";
 
-export default async function MainWeather() {
+interface MainWeatherProps {
+  weatherData: ExtractedWeatherInfo
+}
+
+export default function MainWeather({ weatherData }: MainWeatherProps) {
 
   return (
     <div className="w-full md:max-w-[450px] h-[400px] md:pt-5 bg-transparent md:bg-black/25 flex flex-col gap-y-6 items-center md:justify-start justify-center rounded-xl drop-shadow-[0_30px_10px_rgba(0,0,0,0.1)]">
       <h2 className="hidden md:block text-3xl text-white">Chicago, IL</h2>
-      <Image width={120} height={120} src={`/Sun.png`} alt={`Sun Icon`} />
-      <h1 className="font-bold text-5xl text-white">41 °F</h1>
-      <h4 className="md:hidden text-2xl text-white opacity-70">clear sky</h4>
+      <Image width={120} height={120} src={`/${weatherData.icon}.png`} alt={`${weatherData.icon} Icon`} />
+      <h1 className="font-bold text-5xl text-white">{weatherData.temperature} °F</h1>
+      <h4 className="md:hidden text-2xl text-white opacity-70">{weatherData.weatherDescription}</h4>
       <div className="w-full px-7 py-2 md:py-0 flex flex-row justify-between items-start bg-black/25 rounded-lg md:bg-transparent">
         <div className="flex flex-row gap-x-3">
           <Image
@@ -17,7 +22,7 @@ export default async function MainWeather() {
             src="/Precipitation.png"
             alt="Precipitation Icon"
           />
-          <h5 className="font-light text-white">0%</h5>
+          <h5 className="font-light text-white">{weatherData.precipitation ? weatherData.precipitation : 0}%</h5>
         </div>
         <div className="flex flex-row items-center gap-x-3">
           <Image
@@ -26,7 +31,7 @@ export default async function MainWeather() {
             src="/Humidity.png"
             alt="Humidity Icon"
           />
-          <h5 className="font-light text-white">51%</h5>
+          <h5 className="font-light text-white">{weatherData.humidity}%</h5>
         </div>
         <div className="flex items-center flex-row gap-x-3">
           <Image
@@ -35,11 +40,11 @@ export default async function MainWeather() {
             src="/Wind Speed.png"
             alt="Wind Speed Icon"
           />
-          <h5 className="font-light text-white">4mph</h5>
+          <h5 className="font-light text-white">{weatherData.windSpeed}mph</h5>
         </div>
       </div>
       <h4 className="hidden md:block text-2xl text-white opacity-70">
-        clear sky
+        {weatherData.weatherDescription}
       </h4>
     </div>
   );
