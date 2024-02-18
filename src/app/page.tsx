4378895dfd3ed17545ef78fd5a0cfc5484e9a126
Robert Chapman <chapmanrobert12@gmail.com>
2024-02-18 12:32:36 -0600

    Implemented functionality for the mobile search bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,13 +26,12 @@ export default function Main() {
     )
       .then((res) => res.json())
       .then((weatherData) => setWeatherData(weatherData));
-    console.log(location);
   }, [location]);
 
   return (
     <main className="w-full flex flex-col py-[40px] px-7 md:px-14 justify-start items-center gap-y-10">
       <SearchBar newLocation={newLocation} />
-      <MobileSearchBar />
+      <MobileSearchBar newLocation={newLocation} />
       {weatherData ? (
         <>
           <div className="w-full flex flex-col md:flex-row-reverse justify-center items-center md:gap-x-8 lg:gap-x-16">
